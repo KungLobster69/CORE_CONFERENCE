a5c8f4a108dda81556fc9b23ed95aadc60dfc38a
KungLobster69 <k.lobster69@gmail.com>
2022-12-01 23:29:28 +0700

    update

--- SEED_IV/METHOD01_CODE/ON18_FUZZY_KNN.m
+++ SEED_IV/METHOD01_CODE/ON18_FUZZY_KNN.m
@@ -2,7 +2,7 @@ clc
 clear all
 close all
 
-for File_path = 1:3
+for File_path = 1%:3
     path_name = append('E:\THESIS\RESULT\SEED_IV\METHOD01\16.Edit_Distance_DATA\',num2str(File_path));
     Edit_Distance_DATA_name = append(path_name,'\Edit_Distance_DATA.mat');
     Edit_Distance_DATA_load = load(Edit_Distance_DATA_name);
@@ -11,43 +11,23 @@ for File_path = 1:3
         Edit_Distance_FOLD = Edit_Distance_DATA{Order_K_FOLD,1};
         Class_label_TRAIN = Edit_Distance_DATA{Order_K_FOLD,2};
         Class_label_TEST = Edit_Distance_DATA{Order_K_FOLD,3};
-        % DELETE EMPTY Edit_Distance_FOLD %
-        Edit_Distance_Delete_empty = [];
-        for COL = 1:size(Edit_Distance_FOLD,2)
-            ROW_NEW = 1;
-            for ROW = 1:size(Edit_Distance_FOLD,1)
-                if Edit_Distance_FOLD(ROW,COL) ~= 0
-                    Edit_Distance_Delete_empty(ROW_NEW,COL) = Edit_Distance_FOLD(ROW,COL);
-                    ROW_NEW = ROW_NEW +1;
-                end
-            end
-        end
-        % DELETE EMPTY Class_label_TRAIN %
-        Class_label_TRAIN_Delete_empty = {};
-        ROW_NEW = 1;
-        for ROW = 1:size(Class_label_TRAIN,1)
-            if isempty(Class_label_TRAIN{ROW,1}) == 0
-                Class_label_TRAIN_Delete_empty(ROW_NEW,1) = Class_label_TRAIN(ROW,1);
-                ROW_NEW = ROW_NEW +1;
-            end
-        end
         % Select Sample KNN %
-        Edit_Distance_Sort = sort(Edit_Distance_Delete_empty);
+        Edit_Distance_Sort = sort(Edit_Distance_FOLD);
         KNN = 1;
-        for Number_KNN = 1
+        for Number_KNN = 1:10
             Sample_KNN = [];
             for ROW = 1:KNN
                 Sample_KNN(ROW,:) = Edit_Distance_Sort(ROW,:);
             end
             % deletion %
             Edit_Distance_Deletion = [];
-            for COL = 1:size(Edit_Distance_Delete_empty,2)
-                for ROW_01 = 1:size(Edit_Distance_Delete_empty,1)
+            for COL = 1:size(Edit_Distance_FOLD,2)
+                for ROW_01 = 1:size(Edit_Distance_FOLD,1)
                     for ROW_02 = 1:size(Sample_KNN,1)
-                        if Edit_Distance_Delete_empty(ROW_01,COL) - Sample_KNN(ROW_02,COL) <= 0
+                        if Edit_Distance_FOLD(ROW_01,COL) - Sample_KNN(ROW_02,COL) <= 0
                             Edit_Distance_Deletion(ROW_01,COL) = 0;
                         else
-                            Edit_Distance_Deletion(ROW_01,COL) = Edit_Distance_Delete_empty(ROW_01,COL);
+                            Edit_Distance_Deletion(ROW_01,COL) = Edit_Distance_FOLD(ROW_01,COL);
                         end
                     end
                 end
