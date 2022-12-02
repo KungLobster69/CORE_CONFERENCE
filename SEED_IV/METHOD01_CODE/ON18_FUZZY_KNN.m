c4ebb55ed6b89a28137411fc0ccf676d7869499b
KungLobster69 <k.lobster69@gmail.com>
2022-12-03 02:03:29 +0700

    update

--- SEED_IV/METHOD01_CODE/ON18_FUZZY_KNN.m
+++ SEED_IV/METHOD01_CODE/ON18_FUZZY_KNN.m
@@ -14,7 +14,7 @@ for File_path = 1%:3
         % Select Sample KNN %
         Edit_Distance_Sort = sort(Edit_Distance_FOLD);
         KNN = 1;
-        for Number_KNN = 1:2
+        for Number_KNN = 1:10
             Sample_KNN = [];
             for ROW = 1:KNN
                 Sample_KNN(ROW,:) = Edit_Distance_Sort(ROW,:);
@@ -44,6 +44,7 @@ for File_path = 1%:3
                 end
             end
             % Compare Class Label %
+            ROW_ZERO_LABEL = [];
             for ROW = 1:KNN
                 for COL = 1:size(ROW_ZERO,2)
                     Sample_ROW_ZERO = ROW_ZERO(ROW,COL);
@@ -51,18 +52,59 @@ for File_path = 1%:3
                 end
             end
             % Calcurate u %
+            ROW_U = [];
             m = 2; % default
             Degree = 2/(m-1);
-            for COL = 2%:size(Sample_KNN,2)
-                % CHECK NUMBER %
-                x = unique(ROW_ZERO_LABEL(:,COL));
-                N = numel(x);
-                count = zeros(N,1);
-                for k = 1:N
-                    count(k) = sum(ROW_ZERO_LABEL(:,COL)==x(k));
+            for COL = 1:size(Sample_KNN,2)
+                U_all = 0;
+                U_all_sample = 0;
+                for ROW = 1:size(Sample_KNN,1)
+                    U_all_sample = 1/(Sample_KNN(ROW,COL)^Degree);
+                    U_all = U_all + U_all_sample;
                 end
-                
+                U_0 = 0;
+                U_1 = 0;
+                U_2 = 0;
+                U_3 = 0;
+                U_0_sample = 0;
+                U_1_sample = 0;
+                U_2_sample = 0;
+                U_3_sample = 0;
+                for ROW = 1:size(ROW_ZERO_LABEL,1)
+                    if ROW_ZERO_LABEL(ROW,COL) == 0
+                        U_0_sample = (1/(Sample_KNN(ROW,COL)^Degree))/U_all;
+                        U_0 = U_0 + U_0_sample;
+                    elseif ROW_ZERO_LABEL(ROW,COL) == 1
+                        U_1_sample = (1/(Sample_KNN(ROW,COL)^Degree))/U_all;
+                        U_1 = U_1 + U_1_sample;
+                    elseif ROW_ZERO_LABEL(ROW,COL) == 2
+                        U_2_sample = (1/(Sample_KNN(ROW,COL)^Degree))/U_all;
+                        U_2 = U_2 + U_2_sample;
+                    elseif ROW_ZERO_LABEL(ROW,COL) == 3
+                        U_3_sample = (1/(Sample_KNN(ROW,COL)^Degree))/U_all;
+                        U_3 = U_3 + U_3_sample;
+                    end
+                end
+                Check_sum_U = U_0+U_1+U_2+U_3
+                if U_0 > U_1 && U_0 > U_2 && U_0 > U_3
+                    ROW_U(COL,1) = 0;
+                elseif U_1 > U_0 && U_1 > U_2 && U_1 > U_3
+                    ROW_U(COL,1) = 1;
+                elseif U_2 > U_0 && U_2 > U_1 && U_2 > U_3
+                    ROW_U(COL,1) = 2;
+                elseif U_3 > U_0 && U_3 > U_1 && U_3 > U_2
+                    ROW_U(COL,1) = 3;
+                end
             end
+            % Check Acc %
+            ACC = 0;
+            for ROW = 1:size(Class_label_TEST,1)
+                if Class_label_TEST{ROW,1} == ROW_U(ROW,1)
+                    ACC = ACC+1;
+                end
+            end
+            PERCENT_ACC = (ACC/size(Class_label_TEST,1))*100;
+            PERCENT_ACC_FOLD(Number_KNN,Order_K_FOLD) = PERCENT_ACC;
             KNN = KNN+2;
         end
     end
