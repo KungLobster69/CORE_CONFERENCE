ac492d55c8713265ac15c4420c7c78c312ba103b
KungLobster69 <k.lobster69@gmail.com>
2022-12-02 19:34:45 +0700

    ียกฟะำ

--- SEED_IV/METHOD01_CODE/ON17_KNN.m
+++ SEED_IV/METHOD01_CODE/ON17_KNN.m
@@ -2,7 +2,7 @@ clc
 clear all
 close all
 
-for File_path = 1%:3
+for File_path = 2%:3
     path_name = append('E:\THESIS\RESULT\SEED_IV\METHOD01\16.Edit_Distance_DATA\',num2str(File_path));
     Edit_Distance_DATA_name = append(path_name,'\Edit_Distance_DATA.mat');
     Edit_Distance_DATA_load = load(Edit_Distance_DATA_name);
