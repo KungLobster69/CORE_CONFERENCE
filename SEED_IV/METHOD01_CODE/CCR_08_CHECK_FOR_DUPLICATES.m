630003a18361b902be230a263e37233074215b5f
KungLobster69 <k.lobster69@gmail.com>
2022-12-15 12:00:15 +0700

    update

--- SEED_IV/METHOD01_CODE/CCR_08_CHECK_FOR_DUPLICATES.m
+++ SEED_IV/METHOD01_CODE/CCR_08_CHECK_FOR_DUPLICATES.m
@@ -6,7 +6,7 @@ for File_path = 1:3
     k = [50,60,70,80];
     for COL_K = 1:size(k,2)
         path_name = append('D:\KUNG_LOBSTER69\RESULT\SEED_IV_CONFERENCE\METHOD01\07.CONVERT2STR\',num2str(File_path));
-        TRAIN_STR_name = append(path_name,'\TRAIN_STR_LV3_200_',num2str(k(COL_K)),'.mat');
+        TRAIN_STR_name = append(path_name,'\TRAIN_STR_LV3_100_',num2str(k(COL_K)),'.mat');
         TRAIN_STR_load = load(TRAIN_STR_name);
         TRAIN_STR = TRAIN_STR_load.CONVERT2STR;
         DISTANCE_DUPLICATES = [];
@@ -14,12 +14,12 @@ for File_path = 1:3
             SAMPLE_COL = TRAIN_STR{COL,1};
             for ROW = 1:size(TRAIN_STR,1)
                 SAMPLE_ROW = TRAIN_STR{ROW,1};
-                DISTANCE_DUPLICATES(ROW,COL) = editDistance(SAMPLE_ROW,SAMPLE_COL);
+                DISTANCE_DUPLICATES(ROW,COL) = F01_EditDistance(SAMPLE_ROW,SAMPLE_COL);
             end
         end
         % SAVE_DATA %
         create_path = append('D:\KUNG_LOBSTER69\RESULT\SEED_IV_CONFERENCE\METHOD01\08.DISTANCE_DUPLICATES\',num2str(File_path));
-        create_DISTANCE = append(create_path,'\DISTANCE_DUPLICATES_LV3_200_',num2str(k(COL_K)),'.mat');
+        create_DISTANCE = append(create_path,'\DISTANCE_DUPLICATES_LV3_100_',num2str(k(COL_K)),'.mat');
         mkdir(create_path)
         save(create_DISTANCE,'DISTANCE_DUPLICATES','-v7.3')
     end
